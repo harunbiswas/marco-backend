8676ef98b1493d1e7c0a0056d42338e6490eb5da
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2023-10-14 19:03:33 +0600

    update

--- controler/transport/TransportControler.js
+++ controler/transport/TransportControler.js
@@ -19,16 +19,20 @@ const addTransport = async function (req, res) {
 // get transports
 const getTransport = async function (req, res) {
   const perPage = 30;
-  const { page } = req.query;
+  const { page, search } = req.query;
   const start = (page - 1) * perPage;
   const end = page * [perPage];
 
   try {
-    const result = await Transport.find()
+    const result = await Transport.find({
+      name: { $regex: new RegExp(search, "i") },
+    })
       .skip(start)
       .limit(end - start);
 
-    const count = await Transport.countDocuments();
+    const count = await Transport.countDocuments({
+      name: { $regex: new RegExp(search, "i") },
+    });
     res.status(200).json({ result, count });
   } catch (err) {
     res.status(500).json({
