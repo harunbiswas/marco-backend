c6637fcb33c494f1a9809d8eb85934797a1ffa5f
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2023-10-22 03:27:59 +0600

    Update

--- controler/tag/tagControler.js
+++ controler/tag/tagControler.js
@@ -4,10 +4,34 @@ require("dotenv").config();
 
 const getTagsByKey = async function (req, res) {
   try {
-    const result = await Tag.findOne();
-    console.log("tag results", result);
+    const allTags = await Tag.find({});
 
-    res.status(200).json(result);
+    for (const tag of allTags) {
+      const isTagUsed = await Hotel.exists({
+        $or: [
+          { "offers.tags": tag._id }, // Assuming tags are stored as ObjectIds in the offers array
+          { services: tag.name }, // Assuming services are stored as strings in the services array
+          { strengths: tag.name }, // Assuming strengths are stored as strings in the strengths array
+        ],
+      });
+
+      if (!isTagUsed) {
+        try {
+          const deletedTag = await Tag.findByIdAndDelete(tag._id);
+
+          const result = await Tag.findOne(); // Assuming you want to retrieve a tag after deletion
+          console.log("Tag result", result);
+
+          res.status(200).json(result);
+        } catch (err) {
+          res.status(500).json({
+            errors: {
+              msg: "Internal server error",
+            },
+          });
+        }
+      }
+    }
   } catch (err) {
     res.status(500).json({
       errors: {
